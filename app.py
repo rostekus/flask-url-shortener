c51b33d8f805ce7aedbbd1fc984e176ce8347831
Rostyslav Mosorov <rostyslavmosorov@gmail.com>
2021-05-23 10:46:44 +0200

    flask home page

--- app.py
+++ app.py
@@ -25,4 +25,42 @@ def is_url_in_db(url):
         else:
             return 0
 
+# flask config
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'BzkEv2cCs0'
+
+# Hashids config
+hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
+
+# home page
+@app.route('/', methods=('GET', 'POST'))
+
+def index():
+    conn, curr = get_db_connection()
+    # check method 
+    if request.method == 'POST':
+        #getting user input
+        url = request.form['url']
+
+        if not url:
+            flash('The URL is required!')
+            return redirect(url_for('index'))
+        # inserting url into database
+        url_id = is_url_in_db(url)
+        if url_id ==0:
+
+            with conn:
+                url_data = curr.execute('INSERT INTO urls (original_url) VALUES (?)',
+                                        (url,))
+                conn.commit()
+    
+            url_id = cast_query(url_data)
+        # creating unique 4 character string
+        
+        hashid = hashids.encode(url_id)
+        short_url = request.host_url + hashid
+        # return shorten link 
+        return render_template('index.html', short_url=short_url)
+
+    return render_template('index.html')
 
